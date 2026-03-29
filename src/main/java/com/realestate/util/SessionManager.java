9a497efbacae5cb9959b0180ee84f04f2a117618
Hamza 60103777 <60103777@udst.edu.qa>
2026-03-29 22:50:34 +0300

    Added session manager utility class

--- src/main/java/com/realestate/util/SessionManager.java
+++ src/main/java/com/realestate/util/SessionManager.java
@@ -1,4 +1,27 @@
 package realestate.util;
 
+import realestate.model.User;
+
 public class SessionManager {
+    private static User currentUser;
+
+    public static void setCurrentUser(User currentUser) {
+        SessionManager.currentUser = currentUser;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void clearSession(){
+        currentUser= null;
+    }
+
+    public static boolean isLoggedIn(){
+        return currentUser != null;
+    }
+
+    public static boolean isAdmin(){
+        return currentUser != null && currentUser.isAdmin();
+    }
 }
