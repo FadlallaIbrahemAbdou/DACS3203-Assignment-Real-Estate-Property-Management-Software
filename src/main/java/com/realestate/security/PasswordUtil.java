78cc4449763ecb9b50e446861289e2b2a76ea297
Hamza 60103777 <60103777@udst.edu.qa>
2026-03-29 22:44:36 +0300

    Added password hashing utility class

--- src/main/java/com/realestate/security/PasswordUtil.java
+++ src/main/java/com/realestate/security/PasswordUtil.java
@@ -1,4 +1,12 @@
 package realestate.security;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class PasswordUtil {
+    public static String hashPassword(String plainPassword){
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+    }
+    public static boolean verifyPassword(String plainPassword,String hashedPassword){
+        return BCrypt.checkpw(plainPassword, hashedPassword);
+    }
 }
