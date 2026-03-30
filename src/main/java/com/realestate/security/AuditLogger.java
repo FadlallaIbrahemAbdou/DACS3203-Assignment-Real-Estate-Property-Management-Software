e0c39096282aec61fefbe25a6540bd386f2ace9f
Hamza 60103777 <60103777@udst.edu.qa>
2026-03-30 19:05:47 +0300

    Added Audit Logger security feature class

--- src/main/java/com/realestate/security/AuditLogger.java
+++ src/main/java/com/realestate/security/AuditLogger.java
@@ -1,4 +1,25 @@
 package realestate.security;
 
+import realestate.dao.AuditLogDAO;
+import realestate.model.AuditLog;
+import realestate.util.SessionManager;
+
+
 public class AuditLogger {
+
+    private static final AuditLogDAO auditLogDAO = new AuditLogDAO();
+
+    public static void log(String action, String entityName, Integer entityId, String details){
+        Integer userId = null;
+        if (SessionManager.getCurrentUser() != null){
+            userId = SessionManager.getCurrentUser().getUserId();
+        }
+        AuditLog entry = new AuditLog(userId, action , entityName ,entityId, details);
+        auditLogDAO.insert(entry);
+    }
+
+    public static void log(String action, String entityName, String details){
+        log(action, entityName, null, details);
+    }
+
 }
