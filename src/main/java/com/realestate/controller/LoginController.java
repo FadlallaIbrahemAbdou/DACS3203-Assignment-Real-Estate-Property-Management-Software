8822b8940fa422edba15121c5a779816220b10c3
Fadlalla Ibrahem <60099040@udst.edu.qa>
2026-03-30 21:56:50 +0300

    LoginController Class Added

--- src/main/java/com/realestate/controller/LoginController.java
+++ src/main/java/com/realestate/controller/LoginController.java
@@ -1,4 +1,43 @@
 package realestate.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import realestate.service.AuthService;
+
 public class LoginController {
+
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+    @FXML private Label errorLable;
+
+    private final AuthService authService = new AuthService();
+
+    @FXML
+    private void handlelogin() {
+        String username = usernameField.getText().trim();
+        String password = passwordField.getText();
+
+        String error = authService.login(username, password);
+        if (error != null){
+            errorLable.setText(error);
+            return;
+        }
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/dashboard.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) usernameField.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("REPMS - Dashboard");
+            stage.centerOnScreen();
+        } catch (Exception e) {
+            System.out.println("Error " + e.getMessage());
+            errorLable.setText("Failed to load dashboard");
+        }
+    }
 }
