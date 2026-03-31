f716c81c7a4a4d75fa7ef72a2a445a4138d74451
Hamza 60103777 <60103777@udst.edu.qa>
2026-03-31 21:06:22 +0300

    Added Renter model class

--- src/main/java/com/realestate/model/Renter.java
+++ src/main/java/com/realestate/model/Renter.java
@@ -1,4 +1,66 @@
 package realestate.model;
 
+import java.sql.Timestamp;
+
 public class Renter {
+    private int renterId;
+    private String fullName;
+    private String phone;
+    private String email;
+    private Timestamp createdAt;
+
+    public Renter(){}
+
+    public Renter(String fullName, String phone, String email) {
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getRenterId() {
+        return renterId;
+    }
+
+    public void setRenterId(int renterId) {
+        this.renterId = renterId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Renter{" +
+                "fullName='" + fullName + '\'' +
+                '}';
+    }
 }
