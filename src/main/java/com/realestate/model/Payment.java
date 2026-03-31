ea7a8e5f2449045bebda74b613c0a4ca624f6bad
Hamza 60103777 <60103777@udst.edu.qa>
2026-03-31 23:56:32 +0300

    Added payment model

--- src/main/java/com/realestate/model/Payment.java
+++ src/main/java/com/realestate/model/Payment.java
@@ -1,4 +1,102 @@
 package realestate.model;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
+
 public class Payment {
+
+    private int paymentId;
+    private int leaseId;
+    private Date paymentData;
+    private Date paymentMonth;
+    private BigDecimal amount;
+    private String paymentMethod;
+    private String status;
+    private Timestamp createdAt;
+
+    private String renterName;
+
+    public Payment(){}
+
+    public Payment(int leaseId, Date paymentData, Date paymentMonth, BigDecimal amount, String paymentMethod, String status) {
+        this.leaseId = leaseId;
+        this.paymentData = paymentData;
+        this.paymentMonth = paymentMonth;
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+        this.status = status;
+    }
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getLeaseId() {
+        return leaseId;
+    }
+
+    public void setLeaseId(int leaseId) {
+        this.leaseId = leaseId;
+    }
+
+    public Date getPaymentData() {
+        return paymentData;
+    }
+
+    public void setPaymentData(Date paymentData) {
+        this.paymentData = paymentData;
+    }
+
+    public Date getPaymentMonth() {
+        return paymentMonth;
+    }
+
+    public void setPaymentMonth(Date paymentMonth) {
+        this.paymentMonth = paymentMonth;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getRenterName() {
+        return renterName;
+    }
+
+    public void setRenterName(String renterName) {
+        this.renterName = renterName;
+    }
 }
