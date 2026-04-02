d6d94b281da4bb98397ccafb46c8215dc4370a4a
Hamza 60103777 <60103777@udst.edu.qa>
2026-04-03 00:32:59 +0300

    Applied least-privllege to the repm system, critical issue found during manual SAST

--- src/main/java/com/realestate/util/DBconnection.java
+++ src/main/java/com/realestate/util/DBconnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DBconnection {
 
     private static final String URL= "jdbc:mysql://localhost:3306/repms_db";
-    private static final String USER= "root";
-    private static final String PASSWORD= "";
+    private static final String USER= "hamza";
+    private static final String PASSWORD= "60103777";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
